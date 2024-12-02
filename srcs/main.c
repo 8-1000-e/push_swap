37e3e047257fc15576d33e512bb5ebb0219e75bb
8-1000-e <emiledubdart@gmail.com>
2024-12-02 21:29:01 +0100

    cac

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npalissi <npalissi@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: edubois- <edubois-@student.42angouleme>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 22:12:15 by edubois-          #+#    #+#             */
-/*   Updated: 2024/12/02 20:31:44 by npalissi         ###   ########.fr       */
+/*   Updated: 2024/12/02 20:35:45 by edubois-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,22 @@ int main(int ac, char **av)
 {
 	t_list *a;
 	t_list *a_new;
-	int c_ac;
 
-	a = NULL;
-	av++;
-	c_ac = ac;
-	check_args(ac, av);
-	while (--c_ac)
+	if (ac != 1)
 	{
-		a_new = get_stack(*av);
-		if (!a_new)
-			clean_exit(a, 1);
-		a = list_join(a, a_new);
+		a = NULL;
 		av++;
+		check_args(ac, av);
+		while (--ac)
+		{
+			a_new = get_stack(*av);
+			if (!a_new)
+				clean_exit(a, 1);
+			a = list_join(a, a_new);
+			av++;
+		}
+		check_for_dups(a);
+		start_sort(a);
 	}
-	check_for_dups(a);
-	start_sort(a);
 }
+
