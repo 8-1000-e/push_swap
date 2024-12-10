45a9cab5b863fbb85f25de73a1b087ca7f0772de
naplissi <npalissi@student.42angouleme.fr>
2024-12-10 12:52:26 +0100

    patch quicksort

--- srcs/ft_quicksort/quicksort.c
+++ srcs/ft_quicksort/quicksort.c
@@ -6,7 +6,7 @@
 /*   By: npalissi <npalissi@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 18:46:43 by edubois-          #+#    #+#             */
-/*   Updated: 2024/12/03 02:04:14 by npalissi         ###   ########.fr       */
+/*   Updated: 2024/12/09 14:12:21 by npalissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 
 void	ft_quicksort(t_data *data)
 {
-	if (data->stack_a->content > ft_lstlast(data->stack_a)->content)
-		ra(data);
+
+
 	if (data->stack_a->content > data->stack_a->next->content)
 		sa(data);
+	if (data->stack_a->content > ft_lstlast(data->stack_a)->content)
+		rra(data);
+	if (data->stack_a->next->content > ft_lstlast(data->stack_a)->content)
+	{	
+		ra(data);
+		sa(data);
+		rra(data);
+	}	
 }
